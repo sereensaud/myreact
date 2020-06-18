0a11302bb62f50196f32dbaefa04ea11bd9d73d8
sereensaud <66210596+sereensaud@users.noreply.github.com>
2020-06-19 00:17:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,8 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>THIS IS MY FIRST REACT APP!
-      </h1>
-      <h2>i made it running following commands on cmd</h2>
-      <h3>npm install --global create-react-app
-      </h3>
-      <h3>npx create-react-app myreact</h3>
-      <h3>cd myreact</h3>
-      <h3>npm start</h3>
-      <h2>yaayy!deployment worked  :)</h2>
-    </div>
-  );
+ const name='sereen'
+ return name;
 }
 
 export default App;
